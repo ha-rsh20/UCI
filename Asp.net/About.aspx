a2d66d8d98137a37e7fcc35373481f79898d528b
Harsh Gajjar <88107325+ha-rsh20@users.noreply.github.com>
2024-03-06 16:32:59 +0530

    asp.net application with CRUD operations

---
+++ Asp.net/About.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="testApp.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <div style="display:flex;flex-direction:column">
+            <asp:Label ID="lblStudentName" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="txtStudentName" runat="server" required></asp:TextBox>
+            <asp:Label ID="lblStudentGender" runat="server" Text="Gender"></asp:Label>
+            <asp:TextBox ID="txtStudentGender" runat="server" required></asp:TextBox>
+            <asp:Label ID="lblStudentDob" runat="server" Text="DOB"></asp:Label>
+            <asp:TextBox ID="txtStudentDob" runat="server" type="date" required></asp:TextBox>
+
+            <div style="display:flex;flex-direction:row;margin:10px">
+                <asp:DropDownList ID="ddlCountries" AutoPostBack="true" runat="server" OnSelectedIndexChanged="ddlCountries_SelectedIndexChanged"></asp:DropDownList>
+                <asp:DropDownList ID="ddlStates" AutoPostBack="true" runat="server" OnSelectedIndexChanged="ddlStates_SelectedIndexChanged"></asp:DropDownList>
+                <asp:DropDownList ID="ddlDistricts" runat="server"></asp:DropDownList>
+            </div>
+
+            <asp:Label ID="lblStudentAddress1" runat="server" Text="Address1"></asp:Label>
+            <asp:TextBox ID="txtStudentAddress1" runat="server" required></asp:TextBox>
+            <asp:Label ID="lblStudentAddress2" runat="server" Text="Address2"></asp:Label>
+            <asp:TextBox ID="txtStudentAddress2" runat="server"></asp:TextBox>
+            <asp:Label ID="lblStudentPhone1" runat="server" Text="Phone1"></asp:Label>
+            <asp:TextBox ID="txtStudentPhone1" runat="server" required></asp:TextBox>
+            <asp:Label ID="lblStudentPhone2" runat="server" Text="Phone2"></asp:Label>
+            <asp:TextBox ID="txtStudentPhone2" runat="server"></asp:TextBox>
+            <asp:Label ID="lblStudentEmail" runat="server" Text="Email"></asp:Label>
+            <asp:TextBox ID="txtStudentEmail" runat="server" type="email" required></asp:TextBox>
+            <asp:Label ID="lblStudentPassword" runat="server" Text="Password"></asp:Label>
+            <asp:TextBox ID="txtStudentPassword" runat="server" type="password" required></asp:TextBox>
+        </div>
+        
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" type="submit" OnClick="btnSubmit_Click" style="margin:10px"/>
+    </main>
+</asp:Content>
---
+++ Asp.net/Default.aspx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Drawing;
+using System.Web.DynamicData;
+using System.Xml.Linq;
+
+namespace testApp
+{
+    public partial class _Default : Page
+    {
+        public string _StrCon_Window_Auth = System.Configuration.ConfigurationManager.ConnectionStrings["ConStrWin"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+                lblQueryName.Text = Request.QueryString["name"];
+                lblQueryEmail.Text = Request.QueryString["email"];
+                lblHiddenVal.Text = hfVal.Value;
+                /*if (Session["name"] != null)
+                {
+                    lblSessionName.Text = "Name: "+Session["name"].ToString();
+                }
+                if (Session["email"] != null)
+                {
+                    lblSessionEmail.Text = " Email:"+Session["email"].ToString();
+                }*/
+
+            }
+                /*try
+                {
+                    if (Request.Cookies["name"].Value != null)
+                    {
+                        lblSessionName.Text = "Name: " + Request.Cookies["name"].Value;
+                    }
+                    if (Request.Cookies["email"].Value != null)
+                    {
+                        lblSessionEmail.Text = " Email:" + Request.Cookies["email"].Value;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("cookie isn't set yet!");
+                }*/
+        }
+
+        protected void BindGridView()
+        {
+            SqlConnection _Con = new SqlConnection(_StrCon_Window_Auth);
+            SqlDataAdapter adapter = new SqlDataAdapter("getAllStudents", _Con);
+            DataTable dataTable = new DataTable();
+
+            SqlDataReader rd = null;
+            try
+            {
+                if (_Con.State == ConnectionState.Closed)
+                {
+                    _Con.Open();
+                }
+                adapter.Fill(dataTable);
+
+                gvInfo.DataSource = dataTable;
+                gvInfo.DataBind();
+            }
+            catch (SqlException Ex)
+            {
+
+            }
+            finally
+            {
+                if (_Con.State == ConnectionState.Open)
+                    _Con.Close();
+            }
+        }
+
+        protected void gvInfo_RowCancelingEdit(object sender,GridViewCancelEditEventArgs e)
+        {
+            gvInfo.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void gvInfo_RowDeleting(object sender,GridViewDeleteEventArgs e)
+        {
+            int id = int.Parse(gvInfo.DataKeys[e.RowIndex].Value.ToString());
+            deleteStudentData(id);
+            BindGridView();
+        }
+
+        protected void gvInfo_RowEditing(object sender,GridViewEditEventArgs e)
+        {
+            gvInfo.EditIndex = e.NewEditIndex;
+            BindGridView();
+        }
+
+        protected void gvInfo_RowUpdating(object sender,GridViewUpdateEventArgs e)
+        {
+            int id = int.Parse(gvInfo.DataKeys[e.RowIndex].Value.ToString());
+            TextBox txtName = (TextBox)gvInfo.Rows[e.RowIndex].FindControl("txtName");
+            TextBox txtGender = (TextBox)gvInfo.Rows[e.RowIndex].FindControl("txtGender");
+            TextBox txtDob = (TextBox)gvInfo.Rows[e.RowIndex].FindControl("txtDob");
+            TextBox txtAddress1 = (TextBox)gvInfo.Rows[e.RowIndex].FindControl("txtAddress1");
+            TextBox txtPhone1 = (TextBox)gvInfo.Rows[e.RowIndex].FindControl("txtPhone1");
+            TextBox txtEmail = (TextBox)gvInfo.Rows[e.RowIndex].FindControl("txtEmail");
+
+            updateStudentData(id,txtName.Text, txtGender.Text, txtDob.Text, txtAddress1.Text, txtPhone1.Text, txtEmail.Text);
+            gvInfo.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void updateStudentData(int id,string name,string gender,string dob,string address1,string phone1,string email)
+        {
+            SqlConnection _Con = new SqlConnection(_StrCon_Window_Auth);
+            SqlCommand _cmd = new SqlCommand("updateStudentDataWithoutPassword", _Con);
+            _cmd.Parameters.AddWithValue("name", name);
+            _cmd.Parameters.AddWithValue("gender", gender);
+            _cmd.Parameters.AddWithValue("dob", dob);
+            _cmd.Parameters.AddWithValue("address1", address1);
+            _cmd.Parameters.AddWithValue("phone1", phone1);
+            _cmd.Parameters.AddWithValue("email", email);
+            _cmd.Parameters.AddWithValue("id", id);
+            _cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                if (_Con.State == ConnectionState.Closed)
+                {
+                    _Con.Open();
+                }
+                _cmd.ExecuteNonQuery();
+            }
+            catch (SqlException Ex)
+            {
+                /*Console.WriteLine(Ex);
+                Response.Write(Ex);*/
+            }
+            finally
+            {
+                if (_Con.State == ConnectionState.Open)
+                    _Con.Close();
+            }
+            /*Response.Write($"no {id} updated");*/
+        }
+
+        protected void deleteStudentData(int id)
+        {
+            SqlConnection _Con = new SqlConnection(_StrCon_Window_Auth);
+            SqlCommand _cmd = new SqlCommand("deleteStudentData", _Con);
+            _cmd.Parameters.AddWithValue("id", id);
+            _cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                if (_Con.State == ConnectionState.Closed)
+                {
+                    _Con.Open();
+                }
+                _cmd.ExecuteNonQuery();
+            }
+            catch (SqlException Ex)
+            {
+
+            }
+            finally
+            {
+                if (_Con.State == ConnectionState.Open)
+                    _Con.Close();
+            }
+            /*Response.Write($"no {id} deleted");*/
+        }
+    }
+}
