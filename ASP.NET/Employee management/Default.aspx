be99d4cece8a6d43481fba9365225ec715f0e19b
Harsh Gajjar <88107325+ha-rsh20@users.noreply.github.com>
2024-03-06 16:41:49 +0530

    Basic employee management
    Uses azure blob storage to store images.

---
+++ ASP.NET/Employee management/Default.aspx
@@ -0,0 +1,123 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EmployeeImageUpload._Default" Async="true" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <hr />
+    <div>
+    <div style="font-size: x-large; text-align: center; color: red;"><strong>Add Employee</strong></div>
+    </div>
+    <br />
+    <table class="w-100" style="border-collapse: collapse; width: 100%;">
+    <tr>
+        <td style="padding: 8px; color: #333333;"><span style="font-weight: bold; color: #333333;">First Name</span></td>
+        <td><asp:TextBox ID="textFirstname" runat="server" required="" style="width: 100%;"></asp:TextBox></td>
+    </tr>
+    <tr>
+        <td style="padding: 8px; color: #333333;"><span style="font-weight: bold; color: #333333;">Last Name</span></td>
+        <td><asp:TextBox ID="textLastname" runat="server" required="" style="width: 100%;"></asp:TextBox></td>
+    </tr>
+    <tr>
+        <td style="padding: 8px;"><strong><asp:Label ID="labelDob" runat="server" Text="Data of Birth" style="color: #333333;"></asp:Label></strong></td>
+        <td><asp:TextBox ID="textDob" required="" TextMode="Date" runat="server" style="width: 100%;"></asp:TextBox></td>
+    </tr>
+    <tr>
+        <td style="padding: 5px;"><strong><asp:Label ID="labelGender" runat="server" Text="Gender" style="color: #333333;"></asp:Label></strong></td>
+        <td style="padding: 5px;">
+            <asp:RadioButtonList ID="radioGender" runat="server" RepeatDirection="Horizontal" DataTextField="gender" DataValueField="gender">
+                <asp:ListItem>Female</asp:ListItem>
+                <asp:ListItem>Male</asp:ListItem>
+            </asp:RadioButtonList>
+        </td>
+    </tr>
+    <tr>
+        <td style="padding: 8px;"><strong><asp:Label ID="labelContact" runat="server" Text="Contact" style="color: #333333;"></asp:Label></strong></td>
+        <td><asp:TextBox ID="textContact" TextMode="Phone" required=""  runat="server" style="width: 100%;"></asp:TextBox></td>
+    </tr>
+    <tr>
+        <td style="padding: 8px;"><strong><asp:Label ID="labelAddress" runat="server" Text="Address" style="color: #333333;"></asp:Label></strong></td>
+        <td><asp:TextBox ID="textAddress" required="" runat="server" style="width: 100%;"></asp:TextBox></td>
+    </tr>
+    <tr>
+        <td style="padding: 8px;"><strong><asp:Label ID="labelEmail" runat="server" Text="Email" style="color: #333333;"></asp:Label></strong></td>
+        <td><asp:TextBox ID="textEmail" required=""  runat="server" style="width: 100%;"></asp:TextBox></td>
+    </tr>
+    <tr>
+        <td style="padding: 8px;"><strong><asp:Label ID="labelUsername" runat="server" Text="Username" style="color: #333;"></asp:Label></strong></td>
+        <td><asp:TextBox ID="textUsername" runat="server" required="" style="width: 100%;"></asp:TextBox></td>
+    </tr>
+    <tr>
+        <td style="padding: 8px;"><strong><asp:Label ID="labelImage" runat="server" Text="Photo" style="color: #333;"></asp:Label></strong></td>
+        <td><asp:FileUpload ID="fileImage" runat="server" required="" style="width: 100%;" /></td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td><asp:Button ID="buttonInsert" runat="server" Text="Insert" OnClick="buttonInsert_Click" style="background-color: #007bff; color: #fff; border: none; padding: 8px 16px; cursor: pointer;" Width="174px"/></td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td><strong><asp:Label ID="labelMessage" runat="server" Text="" style="color: #333;"></asp:Label></strong></td>
+    </tr>
+</table>
+
+
+    <br />
+    <div class="col-lg-8" style="margin-top: 30px">
+        <asp:DataList ID="DataList" runat="server" RepeatDirection="Horizontal" RepeatColumns="5" GridLines="Both" OnEditCommand="DataList_EditCommand" OnDeleteCommand="DataList_DeleteCommand">
+         <HeaderStyle BackColor="Black" Font-Bold="true" Font-Size="Large" ForeColor="White" HorizontalAlign="Center" CssClass="header-style" />
+            <HeaderTemplate>
+                <div style="background-color: black; color: white; font-weight: bold; font-size: large; text-align: center;">Employee Data Management</div>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <div style="width:300px; text-align:center; background-color: #f0f0f0; padding: 10px; border-radius: 5px; margin-bottom: 15px;">
+                    <asp:Image ID="image" runat="server" ImageUrl='<%# Eval("image") %>' Width="100px" Height="120px" />
+                    <br />
+                    <asp:Label ID="LabelFirstName" runat="server" Text='<%# Eval("firstname") %>' style="font-weight: bold;"></asp:Label>
+                    <br />
+                    <asp:Label ID="LabelLastName" runat="server" Text='<%# Eval("lastname") %>' style="font-weight: bold;"></asp:Label>
+                    <br />
+                    <asp:Label ID="LabelDOB" runat="server" Text='<%# Eval("dob") %>' style="font-weight: bold;"></asp:Label>
+                    <br />
+                    <asp:Label ID="LabelGender" runat="server" Text='<%# Eval("gender") %>' style="font-weight: bold;"></asp:Label>
+                    <br />
+                    <asp:Label ID="LabelContact" runat="server" Text='<%# Eval("contact") %>' style="font-weight: bold;"></asp:Label>
+                    <br />
+                    <asp:Label ID="LabelAddress" runat="server" Text='<%# Eval("address") %>' style="font-weight: bold;"></asp:Label>
+                    <br />
+                    <asp:Label ID="LabelEmail" runat="server" Text='<%# Eval("email") %>' style="font-weight: bold;"></asp:Label>
+                    <br />
+                    <asp:Label ID="LabelUserName" runat="server" Text='<%# Eval("username") %>' style="font-weight: bold;"></asp:Label>
+                    <br />
+                    <br />
+                    <asp:LinkButton ID="btnEdit" runat="server" CommandName="Edit" CommandArgument='<%# Container.ItemIndex %>' style="background-color: #007bff; color: #fff; border: none; padding: 5px 10px; border-radius: 3px;">Edit</asp:LinkButton>
+                    <asp:LinkButton ID="btnDelete" runat="server" CommandName="Delete" CommandArgument='<%# Container.ItemIndex %>' style="background-color: #dc3545; color: #fff; border: none; padding: 5px 10px; border-radius: 3px; margin-left: 5px;">Delete</asp:LinkButton>
+                </div>
+            </ItemTemplate>
+
+            <EditItemTemplate>
+                <div style="width:150px; text-align:center;">
+                    <br />
+                    <asp:FileUpload ID="fileEditImage" runat="server" />
+                    <br />
+                    <asp:TextBox ID="textEditFirstName" runat="server" Text='<%# Eval("firstname") %>'></asp:TextBox>
+                    <br />
+                    <asp:TextBox ID="textEditLastName" runat="server" Text='<%# Eval("lastname") %>'></asp:TextBox>
+                    <br />
+                    <asp:TextBox ID="textEditDob" runat="server" Text='<%# Eval("dob") %>'></asp:TextBox>
+                    <br />
+                    <asp:TextBox ID="textEditGender" runat="server" Text='<%# Eval("gender") %>'></asp:TextBox>
+                    <br />
+                    <asp:TextBox ID="textEditContact" runat="server" Text='<%# Eval("contact") %>'></asp:TextBox>
+                    <br />
+                    <asp:TextBox ID="textEditAddress" runat="server" Text='<%# Eval("address") %>'></asp:TextBox>
+                    <br />
+                    <asp:TextBox ID="textEditEmail" runat="server" Text='<%# Eval("email") %>'></asp:TextBox>
+                    <br />
+                    <asp:TextBox ID="textEditUserName" runat="server" Text='<%# Eval("username") %>'></asp:TextBox>
+                    <br />
+                    <br />
+                    <asp:LinkButton ID="btnUpdate" runat="server" CommandName="Update" CommandArgument='<%# Container.ItemIndex %>'>Update</asp:LinkButton>
+                    <asp:LinkButton ID="btnCancel" runat="server" CommandName="Cancel">Cancel</asp:LinkButton>
+                </div>
+            </EditItemTemplate>
+        </asp:DataList>
+    </div>
+</asp:Content>
---
+++ ASP.NET/Employee management/Default.aspx.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web.UI.WebControls;
+using Azure.Storage.Blobs;
+
+namespace EmployeeImageUpload
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        string Sql_Auth = System.Configuration.ConfigurationManager.ConnectionStrings["SQL_Auth"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Clear();
+                dataListBind();
+            }
+        }
+
+        protected async void buttonInsert_Click(object sender, EventArgs e)
+        {
+            string blobStorageConStr = "DefaultEndpointsProtocol=https;AccountName=teststoragepro;AccountKey=mEtAsJjw8UJmSsNC9L76eYBeQ9iU8+mlUhG0jPXkUMY0cww6tI1K+Qy11brrhCOcb7PVkwV4D7DA+AStpptjdQ==;EndpointSuffix=core.windows.net";
+            string blobStorageContainerName = "images";
+            BlobContainerClient container = new BlobContainerClient(blobStorageConStr, blobStorageContainerName);
+            
+            
+            if (fileImage.HasFiles)
+            {
+                try
+                {
+                    string extension = Path.GetExtension(fileImage.FileName).ToString();
+                    if (extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension ==".JPG")
+                    {
+                        string filename = Path.GetFileName(fileImage.FileName).ToString();
+                        fileImage.SaveAs(Server.MapPath("Images/") + filename);
+
+                        using (SqlConnection con = new SqlConnection(Sql_Auth))
+                        {
+                            con.Open();
+
+                            //string insertQuery = "INSERT INTO EmployeePersonalDetails (firstname, lastname, dob, gender, contact, address, email, username, image) VALUES (@firstname, @lastname, @dob, @gender, @contact, @address, @email, @username, @image )";
+                            SqlCommand cmd = new SqlCommand("insertEmployee", con);
+                            cmd.CommandType = CommandType.StoredProcedure;
+
+
+                            cmd.Parameters.AddWithValue("@firstname", textFirstname.Text);
+                            cmd.Parameters.AddWithValue("@lastname", textLastname.Text);
+                            cmd.Parameters.AddWithValue("@dob", textDob.Text);
+                            cmd.Parameters.AddWithValue("@gender", radioGender.SelectedItem.Text);
+                            cmd.Parameters.AddWithValue("@contact", textContact.Text);
+                            cmd.Parameters.AddWithValue("@address", textAddress.Text);
+                            cmd.Parameters.AddWithValue("@email", textEmail.Text);
+                            cmd.Parameters.AddWithValue("@username", textUsername.Text);
+
+                            string iPath = "Images/"+filename;
+                            string imagePath = "D:/Projects/Internship Projects/Asp projects/EmployeeImageUpload/Images/" + filename; 
+                            cmd.Parameters.AddWithValue("@image", iPath);
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+
+                            var blob = container.GetBlobClient(iPath);
+                            var stream = File.OpenRead(imagePath);
+                            await blob.UploadAsync(stream);
+                            if (rowsAffected > 0)
+                            {
+                                Response.Write("Data Inserted");
+                                Clear();
+                                dataListBind();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        labelMessage.Text = "Image not in correct format";
+                        labelMessage.ForeColor = System.Drawing.Color.ForestGreen;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    labelMessage.Text = "Error: " + ex.Message;
+                    labelMessage.ForeColor = System.Drawing.Color.ForestGreen;
+                }
+            }
+        }
+
+        protected void DataList_EditCommand(object source, DataListCommandEventArgs e)
+        {
+            DataList.EditItemIndex = e.Item.ItemIndex;
+            Clear();
+            dataListBind();
+        }
+
+        protected void DataList_DeleteCommand(object source, DataListCommandEventArgs e)
+        {
+            string username = ((Label)e.Item.FindControl("LabelUserName")).Text;
+
+            using (SqlConnection con = new SqlConnection(Sql_Auth))
+            {
+                con.Open();
+                //string deleteQuery = "DELETE FROM EmployeePersonalDetails WHERE username = @username";
+                SqlCommand cmd = new SqlCommand("deleteEmployee", con);
+                //cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@username", username);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    string imageUrl = ((Image)e.Item.FindControl("image")).ImageUrl;
+                    string imagePath = Server.MapPath(imageUrl);
+                    if (File.Exists(imagePath))
+                    {
+                        File.Delete(imagePath);
+                    }
+
+                    Response.Write("Data Deleted");
+                    Clear();
+                    dataListBind();
+                }
+            }
+        }
+
+        protected void DataList_UpdateCommand(object source, DataListCommandEventArgs e)
+        {
+            string username = ((Label)e.Item.FindControl("LabelUserName")).Text;
+            TextBox textEditFirstName = (TextBox)e.Item.FindControl("textEditFirstName");
+            TextBox textEditLastName = (TextBox)e.Item.FindControl("textEditLastName");
+            TextBox textEditDob = (TextBox)e.Item.FindControl("textEditDob");
+            TextBox textEditGender = (TextBox)e.Item.FindControl("textEditGender");
+            TextBox textEditContact = (TextBox)e.Item.FindControl("textEditContact");
+            TextBox textEditAddress = (TextBox)e.Item.FindControl("textEditAddress");
+            TextBox textEditEmail = (TextBox)e.Item.FindControl("textEditEmail");
+            TextBox textEditUserName = (TextBox)e.Item.FindControl("textEditUserName");
+            FileUpload fileUploadImage = (FileUpload)e.Item.FindControl("fileUploadImage");
+
+            if (fileUploadImage.HasFile)
+            {
+                string filename = Path.GetFileName(fileUploadImage.FileName);
+                string uploadFolderPath = Server.MapPath("~/Images/"); 
+                string filePath = Path.Combine(uploadFolderPath, filename);
+                fileUploadImage.SaveAs(filePath);
+
+                string imagePath = "Images/" + filename;
+
+                using (SqlConnection con = new SqlConnection(Sql_Auth))
+                {
+                    con.Open();
+                   //string updateImageQuery = "UPDATE EmployeePersonalDetails SET image = @image WHERE username = @username";
+                    SqlCommand cmdImage = new SqlCommand("updateEmployee", con);
+                    cmdImage.CommandType = CommandType.StoredProcedure;
+                    cmdImage.Parameters.AddWithValue("@image", imagePath);
+                    cmdImage.Parameters.AddWithValue("@username", username);
+                    cmdImage.ExecuteNonQuery();
+                }
+            }
+
+            using (SqlConnection con = new SqlConnection(Sql_Auth))
+            {
+                con.Open();
+                string updateQuery = "UPDATE EmployeePersonalDetails SET firstname = @firstname, lastname = @lastname, dob = @dob, gender = @gender, contact = @contact, address = @address, email = @email WHERE username = @username";
+                SqlCommand cmd = new SqlCommand(updateQuery, con);
+
+                cmd.Parameters.AddWithValue("@firstname", textEditFirstName.Text);
+                cmd.Parameters.AddWithValue("@lastname", textEditLastName.Text);
+                cmd.Parameters.AddWithValue("@dob", textEditDob.Text);
+                cmd.Parameters.AddWithValue("@gender", textEditGender.Text);
+                cmd.Parameters.AddWithValue("@contact", textEditContact.Text);
+                cmd.Parameters.AddWithValue("@address", textEditAddress.Text);
+                cmd.Parameters.AddWithValue("@email", textEditEmail.Text);
+                cmd.Parameters.AddWithValue("@username", username);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    Response.Write("Data Updated");
+                    DataList.EditItemIndex = -1;
+                    Clear();
+                    dataListBind();
+                }
+                else
+                {
+                    Response.Write("Data Not Updated");
+                }
+            }
+        }
+
+
+
+        protected void DataList_CancelCommand(object source, DataListCommandEventArgs e)
+        {
+            DataList.EditItemIndex = -1;
+            Clear();
+            dataListBind();
+        }
+
+        public void Clear()
+        {
+            textFirstname.Text = "";
+            textLastname.Text = "";
+            textDob.Text = "";
+            radioGender.ClearSelection();
+            textContact.Text = "";
+            textAddress.Text = "";
+            textEmail.Text = "";
+            textUsername.Text = "";
+            labelMessage.Text = "";
+        }
+
+        protected void dataListBind()
+        {
+            using (SqlConnection con = new SqlConnection(Sql_Auth))
+            {
+                con.Open();
+                string selectQuery = "SELECT * FROM EmployeePersonalDetails";
+                SqlCommand cmd = new SqlCommand(selectQuery, con);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows == true)
+                {
+                    DataList.DataSource = dr;
+                    DataList.DataBind();
+                }
+            }
+        }
+    }
+}
